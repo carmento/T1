5cb054a9acb8f5283c52c555a23fcb2627e764de
Carlos Meneses Torres <carlosmenesestorres@mac-mini-de-carlos.local>
2016-01-13 00:07:16 +0100

    MODIFICACION 2

--- T1E2CarlosMeneses/ViewController.swift
+++ T1E2CarlosMeneses/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("hola")
+        print("hola2")
     }
 
     override func didReceiveMemoryWarning() {
